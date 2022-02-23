34b2a0fb7ce9a8081446bbb4aed86839bcec9e68
BishtGaurav0 <bisht1999gaurav@gmail.com>
2022-02-23 19:57:53 +0530

    small change

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const data = require("./route")
 const cors = require("cors")
-
+const port = 40001;
 const app = express();
 
 
 app.use(cors());
 app.use("/api/v1",data)
 
-app.listen(4001,()=>{
-    console.log("Application started");
+app.listen(process.env.port,()=>{
+    console.log(`  Application started${port}`);
 })
